749a61d47a28aa57ae2a0f51d2d57b1bb1e2b545
Rex <hkchengrex@gmail.com>
2023-10-19 18:56:51 -0500

    download script

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -4,14 +4,14 @@ import hashlib
 from tqdm import tqdm
 
 
-links = [
+_links = [
     ('https://github.com/hkchengrex/Cutie/releases/download/v1.0/coco_lvis_h18_itermask.pth', '6fb97de7ea32f4856f2e63d146a09f31'),
     ('https://github.com/hkchengrex/Cutie/releases/download/v1.0/cutie-base-mega.pth', 'a6071de6136982e396851903ab4c083a'),
 ]
 
 def download_models_if_needed():
     os.makedirs('weights', exist_ok=True)
-    for link, md5 in links:
+    for link, md5 in _links:
         # download file if not exists with a progressbar
         filename = link.split('/')[-1]
         if not os.path.exists(os.path.join('weights', filename)) or hashlib.md5(open(os.path.join('weights', filename), 'rb').read()).hexdigest() != md5:
@@ -27,3 +27,7 @@ def download_models_if_needed():
             t.close()
             if total_size != 0 and t.n != total_size:
                 raise RuntimeError('Error while downloading %s' % filename)
+            
+
+if __name__ == '__main__':
+    download_models_if_needed()
